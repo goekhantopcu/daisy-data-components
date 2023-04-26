05fd7f510191f1591229b257335d74b0d2dcac8c
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-04-26 12:15:55 +0200

    fix(import): export components as default

--- src/table/index.ts
+++ src/table/index.ts
@@ -3,5 +3,4 @@ import DataTable from "./DataTable.vue";
 export * from "./data-table";
 export * from "./DataTable.vue";
 export * from "./data-table-types";
-export default DataTable
 export {DataTable}
--- src/toast/index.ts
+++ src/toast/index.ts
@@ -1,6 +1,6 @@
 import ToastNotification from "./ToastNotification.vue";
 
 export * from './toast-types';
+export * from "./ToastNotification.vue";
 export * from './toast';
-export default ToastNotification
 export {ToastNotification}

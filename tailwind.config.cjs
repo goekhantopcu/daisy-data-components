c473db3729a35fdc89a3e619631762e360fc7ad3
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-05-05 15:14:51 +0200

    fix: import correct content in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: [
+        "modal/*.{vue,js,ts,jsx,tsx}",
+        "table/*.{vue,js,ts,jsx,tsx}",
+        "toast/*.{vue,js,ts,jsx,tsx}",
+        "util/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
 }

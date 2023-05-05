97f9c292903ad78e51b612448c1a80e3efaeec36
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-05-05 15:37:08 +0200

    fix: import correct content in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "modal/modal.ts",
-        "modal/DataModal.vue",
-        "table/DataTable.vue",
-        "table/data-table.ts",
-        "table/data-table-types.ts",
-        "toast/DataToast.vue",
-        "toast/toast.ts",
-        "util/comparison.composable.ts",
-        "util/eventbus.composable.ts",
-        "util/pagination.composable.ts",
-        "util/wildcard.composable.ts",
+        './**/*.{.vue,.ts,.tsx,.js}',
     ],
     theme: {
         extend: {},

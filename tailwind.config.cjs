67b3c9d5e1f9f8029d18933a178a0a87fb1395f7
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-05-05 15:47:58 +0200

    fix: import correct content in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './**/*.{html,js,ts,tsx,vue}',
+        "./index.html",
+        "./**/*.{html,js,ts,tsx,vue}",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "./modal/**/*.{vue,js,ts,jsx,tsx}",
+        "./table/**/*.{vue,js,ts,jsx,tsx}",
+        "./toast/**/*.{vue,js,ts,jsx,tsx}",
+        "./util/**/*.{vue,js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {},

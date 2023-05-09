da95fa5736ff106b9f83981ae9cd21d84bbb1a70
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-05-09 09:54:10 +0200

    fix: event bus registration

--- util/eventbus.composable.ts
+++ util/eventbus.composable.ts
@@ -3,11 +3,13 @@ import {v4} from 'uuid';
 export type EventCallback<T> = (data: T) => void;
 
 export class EventBus<T> {
-    private readonly key: string;
+    active: boolean;
+    readonly key: string;
     private readonly listeners: Map<string, EventCallback<T>>;
 
     constructor(key: string) {
         this.key = key;
+        this.active = true;
         this.listeners = new Map<string, EventCallback<T>>();
     }
 
@@ -29,26 +31,29 @@ export class EventBus<T> {
     }
 
     public destroy() {
+        this.active = false;
         this.listeners.clear();
-        EVENT_BUS_INSTANCES.delete(this.key);
         console.log(`Eventbus(action='destroy', key='${this.key}', size='${Array(this.listeners.keys()).length}')`);
     }
 }
 
 export const DEFAULT_GENERAL_EVENT_BUS_KEY = 'daisy_data_general_event_bus';
-export const EVENT_BUS_INSTANCES = new Map<string, EventBus<any>>;
+export const EVENT_BUS_INSTANCES: EventBus<any>[] = [];
 
 export function useEventBus<T>(key?: string): EventBus<T> {
     if (key === undefined) {
         return useEventBus(DEFAULT_GENERAL_EVENT_BUS_KEY);
     }
-    const cached = EVENT_BUS_INSTANCES.get(key);
+    const cached = EVENT_BUS_INSTANCES.find(value => value.key === key);
     if (cached) {
+        if (!cached.active) {
+            throw new Error('The requested event-bus was disabled');
+        }
         console.log(`Eventbus(action='cached', key='${key}', size='${Array(EVENT_BUS_INSTANCES.keys()).length}')`);
         return cached;
     }
     const bus = new EventBus<T>(key);
     console.log(`Eventbus(action='register', key='${key}', size='${Array(EVENT_BUS_INSTANCES.keys()).length}')`);
-    EVENT_BUS_INSTANCES.set(key, bus);
+    EVENT_BUS_INSTANCES.push(bus);
     return bus;
 }

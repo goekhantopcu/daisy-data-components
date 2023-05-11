48922121e37ed3c07a1ff5f6ac6d886a73f582a7
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-05-11 08:36:10 +0200

    feat: default close button to modal

--- modal/DataModal.vue
+++ modal/DataModal.vue
@@ -9,31 +9,52 @@
            @change="trigger($event)" />
     <section class="modal modal-middle" :class="wrapperClasses">
       <article class="modal-box" :class="outerClasses">
-        <slot name="content" aria-label="Modal-Content" :modal="modal"></slot>
-        <div v-if="$slots.hasOwnProperty('action')" class="modal-action" :class="actionClasses">
+        <section v-if="hasHeader || hasHeaderClose || addHeaderClose" class="modal-header" aria-label="Modal-Header" :class="headerClasses">
+          <slot v-if="hasHeader" name="header" :modal="modal"></slot>
+          <div v-else class="ml-auto" tabindex="0">
+            <slot v-if="hasHeaderClose" name="header-close" :modal="modal"></slot>
+            <button v-else class="h-6 p-1" aria-label="Modal-Close" @click="modal.close()" :class="headerCloseClasses">
+              <svg class="modal-close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
+            </button>
+          </div>
+        </section>
+
+        <slot v-if="hasContent" name="content" aria-label="Modal-Content" :modal="modal"></slot>
+
+        <section v-if="hasAction" class="modal-action" :class="actionClasses">
           <slot name="action" aria-label="Modal-Actions" :modal="modal"></slot>
-        </div>
+        </section>
       </article>
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
-import {onBeforeUnmount, onMounted, onUnmounted, ref} from "vue";
+import {computed, onBeforeUnmount, onMounted, onUnmounted, ref, useSlots} from "vue";
 import {useModal} from "./modal";
 import {useEmitter} from "../emitter";
 
 const props = defineProps<{
   id: string;
+  addHeaderClose?: boolean;
   wrapperClasses?: string;
   outerClasses?: string;
+  headerClasses?: string;
   actionClasses?: string;
+  headerCloseClasses?: string;
 }>();
 
 const emitter = useEmitter();
 const modal = useModal(props.id);
 const checkbox = ref<HTMLInputElement>();
 const emits = defineEmits(['modal:close', 'modal:open']);
+
+const slots = useSlots();
+
+const hasAction = computed(() => slots.hasOwnProperty('action'));
+const hasHeader = computed(() => slots.hasOwnProperty("header"));
+const hasContent = computed(() => slots.hasOwnProperty('content'));
+const hasHeaderClose = computed(() => slots.hasOwnProperty("header-close"));
 
 function trigger(event: any) {
   const target = event.target as HTMLInputElement;
@@ -88,4 +109,11 @@ onBeforeUnmount(() => {
 onUnmounted(() => modal.destroy());
 </script>
 
-<style scoped></style>
+<style scoped>
+.modal-header {
+  @apply flex flex-row justify-between items-center
+}
+.modal-close-icon {
+  @apply h-6 fill-neutral-content
+}
+</style>

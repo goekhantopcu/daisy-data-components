4b99130e81ec65e84ca88c346945627dc585d6de
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-05-11 14:05:56 +0200

    fix: tiny modal close icon

--- modal/DataModal.vue
+++ modal/DataModal.vue
@@ -13,8 +13,8 @@
           <slot v-if="hasHeader" name="header" :modal="modal"></slot>
           <div v-if="addHeaderClose || hasHeaderClose" class="ml-auto" tabindex="0">
             <slot v-if="hasHeaderClose" name="header-close" :modal="modal"></slot>
-            <button v-else class="h-6 p-1" aria-label="Modal-Close" @click="modal.close()" :class="headerCloseClasses">
-              <svg class="h-6 fill-neutral-content" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
+            <button v-else class="h-5 p-1" aria-label="Modal-Close" @click="modal.close()" :class="headerCloseClasses">
+              <svg class="h-5 fill-neutral-content" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
             </button>
           </div>
         </section>
